b4fe370aa6f633fd32faa5fbd4a2effd4ba0cf55
SAKSHAM <sakshrwt@gmail.com>
2024-10-10 17:55:13 +0530

    Updated ui for mobile screens

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,16 +1,58 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div className="flex flex-row justify-between items-center bg-gradient-to-br from-blue-600 to-blue-700 px-20 py-6">
-      <h1 className='text-3xl text-center font-marker text-white select-none'>Talk Tuah</h1>
-      <ul className="flex flex-row justify-between items-center gap-10">
-        <Link to="/"><li className='cursor-pointer select-none text-white font-space font-semibold text-lg hover:underline'>Home</li></Link>
-        <li className='cursor-pointer select-none text-white font-space font-semibold text-lg hover:underline'>About</li>
-        <li className='cursor-pointer select-none text-white font-space font-semibold text-lg hover:underline'>Connect</li>
-      </ul>
-    </div>
-  )
-}
+  const [pageSize, setPageSize] = useState<number | undefined>(undefined);
 
-export default Header
+  useEffect(() => {
+    const updatePageSize = () => {
+      setPageSize(document.getElementById('root')?.clientWidth);
+    };
+
+    updatePageSize();
+    window.addEventListener('resize', updatePageSize);
+
+    return () => window.removeEventListener('resize', updatePageSize);
+  }, []);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  if (pageSize !== undefined && pageSize < 768) {
+    return (
+      <div>
+        <div className="flex flex-row justify-between items-center bg-gradient-to-br from-blue-600 to-blue-700 px-10 py-6">
+          <h1 className='text-3xl text-center font-marker text-white select-none'>Talk Tuah</h1>
+          <div className="flex flex-col gap-1.5 cursor-pointer" onClick={toggleMenu}>
+            <div className="h-1 w-8 bg-white"></div>
+            <div className="h-1 w-8 bg-white"></div>
+            <div className="h-1 w-8 bg-white"></div>
+          </div>
+        </div>
+        {isMenuOpen && (
+          <ul className="absolute right-0 text-right space-y-4 mr-4 mt-4 backdrop-blur-md p-6 rounded-3xl border-2 border-gray-300">
+            <Link to="/"><li className='cursor-pointer select-none font-space font-semibold text-lg hover:underline'>Home</li></Link>
+            <li className='cursor-pointer select-none font-space font-semibold text-lg hover:underline'>About</li>
+            <li className='cursor-pointer select-none font-space font-semibold text-lg hover:underline'>Connect</li>
+          </ul>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-row justify-between items-center bg-gradient-to-br from-blue-600 to-blue-700 px-14 py-6">
+        <h1 className='text-3xl text-center font-marker text-white select-none'>Talk Tuah</h1>
+        <ul className="flex flex-row justify-between items-center gap-10">
+          <Link to="/"><li className='cursor-pointer select-none text-white font-space font-semibold text-lg hover:underline'>Home</li></Link>
+          <li className='cursor-pointer select-none text-white font-space font-semibold text-lg hover:underline'>About</li>
+          <li className='cursor-pointer select-none text-white font-space font-semibold text-lg hover:underline'>Connect</li>
+        </ul>
+      </div>
+    );
+  }
+};
+
+export default Header;
--- client/src/pages/genre/Blockchain.tsx
+++ client/src/pages/genre/Blockchain.tsx
@@ -4,7 +4,7 @@ const Blockchain = () => {
   return (
     <div>
       <Header />
-      Blockchain
+      Blockchain Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, sit! Voluptatem recusandae tempore non velit necessitatibus provident unde impedit deserunt cum. Quae dolores distinctio molestiae soluta rem. A, earum aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas architecto, fugiat incidunt beatae natus nobis. Temporibus voluptates repudiandae cum rem harum aliquam a distinctio, ipsa quia, explicabo dicta consequuntur modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur eligendi a nemo eius tempore dolores ad quibusdam hic, libero omnis impedit deserunt, sint, magni amet sequi esse? Eligendi, eum nam!
     </div>
   )
 }
