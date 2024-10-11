45f8d3f5dcd4ef4a92bb24d5830ccbe62d04f17f
SAKSHAM <sakshrwt@gmail.com>
2024-10-11 11:32:11 +0530

    Update UI for mobile screens in Header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -27,13 +27,13 @@ const Header = () => {
         <div className="flex flex-row justify-between items-center bg-gradient-to-br from-blue-600 to-blue-700 px-10 py-6">
           <h1 className='text-3xl text-center font-marker text-white select-none'>Talk Tuah</h1>
           <div className="flex flex-col gap-1.5 cursor-pointer" onClick={toggleMenu}>
-            <div className="h-1 w-8 bg-white"></div>
-            <div className="h-1 w-8 bg-white"></div>
-            <div className="h-1 w-8 bg-white"></div>
+            <div className={`h-1 w-8 bg-white transition-transform duration-300 ${isMenuOpen ? 'transform rotate-45 translate-y-3' : ''}`}></div>
+            <div className={`h-1 w-8 bg-white transition-opacity duration-300 ${isMenuOpen ? 'opacity-0' : ''}`}></div>
+            <div className={`h-1 w-8 bg-white transition-transform duration-300 ${isMenuOpen ? 'transform -rotate-45 -translate-y-2' : ''}`}></div>
           </div>
         </div>
         {isMenuOpen && (
-          <ul className="absolute right-0 text-right space-y-4 mr-4 mt-4 backdrop-blur-md p-6 rounded-3xl border-2 border-gray-300">
+          <ul className={`absolute z-10 right-0 text-right space-y-4 mr-4 mt-4 backdrop-blur-md p-6 rounded-3xl border-2 border-gray-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`}>
             <Link to="/"><li className='cursor-pointer select-none font-space font-semibold text-lg hover:underline'>Home</li></Link>
             <li className='cursor-pointer select-none font-space font-semibold text-lg hover:underline'>About</li>
             <li className='cursor-pointer select-none font-space font-semibold text-lg hover:underline'>Connect</li>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { faWallet } from '@fortawesome/free-solid-svg-icons';
 const Home = () => {
   return (
       <div>
-        <img loading='lazy' src="https://raw.githubusercontent.com/jny0444/talk-tuah/refs/heads/main/client/src/assets/star.gif" className="fixed min-h-screen -z-10 object-cover"></img>
+        <img src="https://raw.githubusercontent.com/jny0444/talk-tuah/refs/heads/main/client/src/assets/star.gif" className="fixed min-h-screen -z-10 object-cover"></img>
         <div className="flex flex-col justify-center items-center backdrop-blur-md min-h-screen gap-8">
           <h1 className='text-9xl text-center font-marker text-white select-none max-sm:scale-90'>Talk Tuah</h1>
           <p className='text-xl text-center font-pacifico text-white select-none max-sm:scale-90'>with Anon</p>
