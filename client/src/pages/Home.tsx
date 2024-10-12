1cd1c734787bef61458a8b1bcdf7e56a76b59665
SAKSHAM <sakshrwt@gmail.com>
2024-10-13 00:46:49 +0530

    Made a small mistake

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import Header from '@/components/Header';
 import { Card } from '@/components/Card';
 import Footer from '@/components/Footer';
