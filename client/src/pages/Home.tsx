73a31b99239cd3528ea877358e566381655532e5
SAKSHAM <sakshrwt@gmail.com>
2024-10-18 00:31:38 +0530

    Comment

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -22,8 +22,6 @@ const Home = () => {
     };
   }, []);
 
-  // Hello
-
   return (
     <div className='background space-y-56 max-2xl:space-y-52 max-xl:space-y-48 max-lg:space-y-44 max-md:space-y-36 max-sm:space-y-24'>
       <Header/>
