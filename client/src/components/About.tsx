6ddff95c5ee6fb733f1230fd97d8591e84d43138
SAKSHAM <sakshrwt@gmail.com>
2024-11-01 13:21:31 +0530

    Added a donut

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,9 +1,15 @@
+import Spline from '@splinetool/react-spline';
+
 const About = () => {
   return (
-    <div className="flex flex-col items-center gap-8 mx-10 p-5 rounded-xl md:mx-20 lg:mx-36 xl:gap-16 xl:mx-52">
-      <h3 className="font-lexend text-4xl xl:text-5xl">About</h3>
-      <p className="font-lexend text-justify md:text-lg lg:text-xl xl:text-2xl">Talk Twoah is a decentralized, blockchain-powered forum inspired by 4chan’s freeform, anonymous style of discussion, but with enhanced privacy and security. Every post, comment, and interaction is permanently stored on the blockchain, giving users full control and ensuring content cannot be altered or removed. Unlike traditional platforms, Talk Twoah operates without a central authority, allowing for open, uncensored conversations in real time. Whether sharing memes, discussing niche topics, or diving into serious debates, Talk Twoah combines the unfiltered nature of 4chan with the transparency and trust of blockchain technology. Join us in redefining online discourse where every voice matters and content is protected.</p>
+    <div className='relative'>
+      <div className="flex flex-col items-center gap-8 py-20 px-10 p-5 rounded-xl md:px-20 lg:px-36 xl:gap-16 xl:px-52 xl:py-40">
+        <h3 className="font-lexend text-4xl xl:text-5xl">About</h3>
+        <p className="font-lexend text-justify md:text-lg lg:text-xl xl:text-2xl">Talk Twoah is a decentralized, blockchain-powered forum inspired by 4chan’s freeform, anonymous style of discussion, but with enhanced privacy and security. Every post, comment, and interaction is permanently stored on the blockchain, giving users full control and ensuring content cannot be altered or removed. Unlike traditional platforms, Talk Twoah operates without a central authority, allowing for open, uncensored conversations in real time. Whether sharing memes, discussing niche topics, or diving into serious debates, Talk Twoah combines the unfiltered nature of 4chan with the transparency and trust of blockchain technology. Join us in redefining online discourse where every voice matters and content is protected.</p>
+      </div>
+      <Spline scene="https://prod.spline.design/Tu37aCxLmALVRj-t/scene.splinecode" className='-z-10 absolute'/>
     </div>
+
   )
 }
 export default About
